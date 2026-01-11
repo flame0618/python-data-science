2e6378207557810e843ba6ad4fafc684f18a22f3
Edward Guwalgiya <edward@mac.home>
2026-01-11 12:57:25 +0100

    Lesson 1: Python basics

---
+++ lesson1_basics.py
@@ -0,0 +1,19 @@
+# Lesson 1: Python basics
+
+age = 30
+height = 1.75
+name = "YourName"
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# TODO:
+# 1. Print the type of each variable
+print(type(age),type(height),type(name))
+# 2. Calculate age in months
+age_in_months = age*12
+print("age in months:", age_in_months)
+# 3. Store whether age > 18 in a variable called is_adult
+ is_adult = age>18
+print("Is adult:", is_adult)
